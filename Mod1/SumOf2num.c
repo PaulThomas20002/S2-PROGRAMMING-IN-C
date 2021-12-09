4b513333b50c62801ba508be875ec4067bb7095c
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2021-12-09 18:44:49 +0530

    Create SumOf2num.c

---
+++ Mod1/SumOf2num.c
@@ -0,0 +1,21 @@
+/*(ii) Read two numbers add them and display their sum 
+AIM 
+To find the sum of two numbers 
+ALGORITHM 
+Step 1 : Start 
+Step 2 : Input the values of variables a, b 
+Step 3 : Store the sum of a and b in variable sum Step 4 : Print the variable sum 
+Step 5 : Stop 
+PROGRAM */
+
+
+#include <stdio.h> 
+int main() 
+{ 
+float a, b, sum; 
+printf("Enter two numbers \n"); 
+scanf("%f %f", &a, &b); 
+sum=a+b; 
+print ("Sum of numbers = %f", sum); 
+return 0; 
+}
