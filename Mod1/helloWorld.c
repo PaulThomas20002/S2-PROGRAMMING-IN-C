8230a90384f2b8a402ff764b881cb0dca9d4e0a5
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2021-12-09 18:43:29 +0530

    Create helloWorld.c

---
+++ Mod1/helloWorld.c
@@ -0,0 +1,15 @@
+/*(i) Display “Hello World” 
+AIM 
+To display “Hello world”. 
+ALGORITHM 
+Step 1 : Start 
+Step 2 : Print “Hello world” 
+Step 3 : Stop 
+PROGRAM */
+
+#include <stdio.h> 
+int main() 
+{ 
+printf("Hello World"); 
+return 0; 
+}
